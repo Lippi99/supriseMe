d1c0f9f982037a6cd9ad68c7d228be8ef3b67d06
Felipe Salomao <lipesalomaoclaf@gmail.com>
2024-12-06 16:48:06 -0300

    feat: added preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
+  nitro: {
+    preset: "cloudflare-pages",
+  },
   plugins: [
     {
       src: "~/plugins/confetti.client",
