eda2f8eb0a4f027ee74068c4d7dfec715d0cce33
Felipe Salomao <lipesalomaoclaf@gmail.com>
2024-12-11 20:35:22 -0300

    fix: hidden

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,16 +1,16 @@
 import GoogleProvider from "next-auth/providers/google";
-import { NuxtAuthHandler } from "#auth";
+// import { NuxtAuthHandler } from "#auth";
 
 const config = useRuntimeConfig();
 
-export default NuxtAuthHandler({
-  secret: config.auth.secret,
-  providers: [
-    // @ts-expect-error Use .default here for it to work during SSR.
-    GoogleProvider.default({
-      clientId: config.auth.providers.google.client,
-      clientSecret: config.auth.providers.google.secret,
-    }),
-  ],
-  callbacks: {},
-});
+// export default NuxtAuthHandler({
+//   secret: config.auth.secret,
+//   providers: [
+//     // @ts-expect-error Use .default here for it to work during SSR.
+//     GoogleProvider.default({
+//       clientId: config.auth.providers.google.client,
+//       clientSecret: config.auth.providers.google.secret,
+//     }),
+//   ],
+//   callbacks: {},
+// });
