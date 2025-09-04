5c56e814de4566b2d2d2c615d0db38b0340b136f
Lippi99 <lipesalomao99@gmail.com>
2025-09-04 16:58:55 -0300

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,50 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+
+        // Apple devices
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+
+        // Android devices
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/android-chrome-192x192.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "512x512",
+          href: "/android-chrome-512x512.png",
+        },
+
+        // Web App Manifest
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
   nitro: {
     preset: "node-server",
     experimental: {
